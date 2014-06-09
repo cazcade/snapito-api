86843cec1020f80b181f386c0a0cb4aa6b1e8c4e
Neil Ellis <neil@cazcade.com>
2014-06-09 17:31:21 +0100

    Initial work

---
+++ src/test/java/com/cazcade/snapitoapi/test/RunApiTests.java
@@ -0,0 +1,32 @@
+package com.cazcade.snapitoapi.test;
+
+import com.jayway.restassured.RestAssured;
+import com.jayway.restassured.builder.ResponseSpecBuilder;
+import com.jayway.restassured.specification.ResponseSpecification;
+import org.junit.Test;
+
+import java.util.Arrays;
+
+import static com.jayway.restassured.RestAssured.given;
+
+/**
+ * @author <a href="http://uk.linkedin.com/in/neilellis">Neil Ellis</a>
+ * @todo document.
+ */
+public class RunApiTests {
+    public static final String env = System.getProperty("target.subdomain", "api");
+    public static final String apiHost = "http://" + env + ".snapito.com/";
+
+    @Test
+    public void test() {
+        RestAssured.baseURI = apiHost;
+        ResponseSpecBuilder validImageBuilder = new ResponseSpecBuilder();
+        validImageBuilder.expectStatusCode(200);
+        validImageBuilder.expectContentType("image/png");
+        ResponseSpecification validImage = validImageBuilder.build();
+
+        for(String host : Arrays.asList("google.com", "bbc.com", "cnn.com") ) {
+            given().param("url", host).when().get().then().spec(validImage);
+        }
+    }
+}
